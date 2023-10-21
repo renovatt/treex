100a21d4192195784be0b3ea26b6f95918210662
will <wlymes@gmail.com>
2023-10-21 10:54:25 -0300

    feat: middleware added

---
+++ src/middleware.ts
@@ -0,0 +1,32 @@
+import { NextResponse, NextRequest } from 'next/server'
+
+export default function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname
+  const token = request.cookies.get('@auth_accessToken')?.value || ''
+
+  const isPublicPath =
+    path === '/' ||
+    path === '/login' ||
+    path === '/register' ||
+    path === '/recovery'
+
+  if (isPublicPath && token) {
+    return NextResponse.redirect(new URL('/dashboard', request.nextUrl))
+  }
+
+  if (!isPublicPath && !token) {
+    return NextResponse.redirect(new URL('/login', request.nextUrl))
+  }
+}
+
+export const config = {
+  matcher: [
+    '/',
+    '/login/:path*',
+    '/notes/:path*',
+    '/register/:path*',
+    '/recovery/:path*',
+    '/dashboard/:path*',
+    '/transactions/:path*',
+  ],
+}
