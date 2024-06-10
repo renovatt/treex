14140ceb31f793afc2a2c06d390397500eebdf38
will <wlymes@gmail.com>
2024-06-10 08:59:31 -0300

    feat: sidebar store

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,7 +3,7 @@ import { DateStore, UseStoreProps } from './types'
 
 const useSideBarStore = create<UseStoreProps>((set) => ({
   status: {
-    isSelected: false,
+    isSelected: true,
   },
   setStatus: () => {
     set((state) => {
