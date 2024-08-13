e80a5df8ecc769a72b9e6115ea2052193a5bf70c
will <wlymes@gmail.com>
2024-08-13 09:15:04 -0300

    feat: adding tooltip

--- src/components/features/dashboard/charts/overview-bar-chart.tsx
+++ src/components/features/dashboard/charts/overview-bar-chart.tsx
@@ -1,8 +1,21 @@
 'use client'
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from '@/components/ui/chart'
 import { useGetTransactions } from '@/hooks/useGetTransactions'
 import { UserData } from '@/lib/types'
 import { calculateRevenueByMonth } from '@/utils/calculate-revenue-by-month'
-import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from 'recharts'
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  ResponsiveContainer,
+  XAxis,
+  YAxis,
+} from 'recharts'
 
 export function OverviewBarChart({ user }: { user: UserData }) {
   const { transactionData } = useGetTransactions(user)
@@ -13,30 +26,49 @@ export function OverviewBarChart({ user }: { user: UserData }) {
     total: item.revenue,
   }))
 
+  const chartConfig = {
+    desktop: {
+      label: 'total',
+      color: 'hsl(var(--chart-1))',
+    },
+  } satisfies ChartConfig
+
   return (
     <ResponsiveContainer width="100%" height={350}>
-      <BarChart data={data}>
-        <XAxis
-          dataKey="name"
-          stroke="#888888"
-          fontSize={12}
-          tickLine={false}
-          axisLine={false}
-        />
-        <YAxis
-          stroke="#888888"
-          fontSize={12}
-          tickLine={false}
-          axisLine={false}
-          tickFormatter={(value) => `$${value}`}
-        />
-        <Bar
-          dataKey="total"
-          fill="currentColor"
-          radius={[4, 4, 0, 0]}
-          className="fill-primary"
-        />
-      </BarChart>
+      <ChartContainer config={chartConfig}>
+        <BarChart accessibilityLayer data={data}>
+          <CartesianGrid vertical={false} />
+          <XAxis
+            dataKey="month"
+            tickLine={false}
+            tickMargin={10}
+            axisLine={false}
+            tickFormatter={(value) => value.slice(0, 3)}
+          />
+          <YAxis
+            stroke="#888888"
+            fontSize={9}
+            tickLine={false}
+            axisLine={false}
+            tickFormatter={(value) =>
+              `${value.toLocaleString('pt-BR', {
+                style: 'currency',
+                currency: 'BRL',
+              })}`
+            }
+          />
+          <ChartTooltip
+            cursor={false}
+            content={<ChartTooltipContent hideLabel />}
+          />
+          <Bar
+            dataKey="total"
+            fill="currentColor"
+            radius={[8, 8, 0, 0]}
+            className="fill-primary"
+          />
+        </BarChart>
+      </ChartContainer>
     </ResponsiveContainer>
   )
 }
