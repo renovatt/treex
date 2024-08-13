b159636b80ab3f4a854d9d2cb13233726ddc3e30
will <wlymes@gmail.com>
2024-08-13 09:27:14 -0300

    feat: error message schema

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -2,9 +2,12 @@ import { z } from 'zod'
 
 export const TransactionSchema = z.object({
   id: z.string().optional(),
-  name: z.string().min(3, 'Nome obrigatório').max(24, 'Nome é muito longo'),
+  name: z
+    .string({ required_error: 'Nome obrigatório' })
+    .min(3, 'Nome obrigatório')
+    .max(24, 'Nome é muito longo'),
   value: z
-    .string()
+    .string({ required_error: 'Valor obrigatório' })
     .min(1, 'Valor obrigatório')
     .refine(
       (value: string) => {
@@ -23,16 +26,22 @@ export const TransactionSchema = z.object({
 
 export const PrioritySchema = z.object({
   id: z.string().optional(),
-  name: z.string().min(3, 'Nome obrigatório').max(24, 'Nome é muito longo'),
+  name: z
+    .string({ required_error: 'Nome obrigatório' })
+    .min(3, 'Nome obrigatório')
+    .max(24, 'Nome é muito longo'),
   level: z.enum(['Importante', 'Menos importante', 'Muito importante']),
   date: z.string().optional(),
 })
 
 export const MonthyPreviewSchema = z.object({
   id: z.string().optional(),
-  name: z.string().min(3, 'Nome obrigatório').max(24, 'Nome é muito longo'),
+  name: z
+    .string({ required_error: 'Nome obrigatório' })
+    .min(3, 'Nome obrigatório')
+    .max(24, 'Nome é muito longo'),
   value: z
-    .string()
+    .string({ required_error: 'Valor obrigatório' })
     .min(1, 'Valor obrigatório')
     .refine(
       (value: string) => {
